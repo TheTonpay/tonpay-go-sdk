5cd2ac07a6474175f2cadda7332e3ae8fdc10ef7
VadimGerman <los.polos2015@gmail.com>
2023-04-12 17:57:47 +0400

    feat: upgrade example with precalculate contract methods

--- examples/create-and-pay-invoice-url/main.go
+++ examples/create-and-pay-invoice-url/main.go
@@ -9,16 +9,43 @@ import (
 )
 
 func main() {
-	storeAddr := address.MustParseAddr("EQCA_bhKoqvxxG44_fBDC4VFQ36Sdzu6i8KaD2SMaZwWS_N9")
+	owner := "EQASwvTkaA3TyA-OFyhj2-H7pX5ShRbA0mD3dLRGhayvoOtd"
+	store := "EQBeEgLLL369qn-SK8ikE2FzX9MnblI_RDP5WLxCHm6Ihl_o"
+	storeAddr := address.MustParseAddr(store)
 	amount := tlb.MustFromTON("1").NanoTON().Uint64()
+	invoiceID := "created using tonpaygo, paid by wallet"
 
-	requestCell, err := tonpaygo.RequestPurchaseMessage("created using tonpaygo, paid by wallet", amount)
+	// Building request cell. It will be used to generate url for the wallet
+	requestCell, err := tonpaygo.RequestPurchaseMessage(invoiceID, "", amount)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
+	// Building invoice config. It will be used to calculate invoice address
+	config := tonpaygo.InvoiceData{
+		Store:       store,
+		Merchant:    owner,
+		Beneficiary: "EQD2wz8Rq5QDj9iK2Z_leGQu-Rup__y-Z4wo8Lm7-tSD6Iz2",
+		HasCustomer: false,
+		Customer:    tonpaygo.ZeroAddress,
+		InvoiceID:   invoiceID,
+		Metadata:    "",
+		Amount:      amount,
+		Paid:        false,
+		Active:      true,
+	}
+
+	// Calculating invoice address
+	invoiceAddress, err := tonpaygo.PrecalculateInvoiceAddress(0, config)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Building url for the wallet
 	url := tonpaygo.BuildURLForWallet(storeAddr.String(), requestCell, amount, tonpaygo.RequestPurchaseFee)
 
-	fmt.Printf("Send this url to the customer: %s", url)
+	fmt.Println("Send this url to the customer:", url)
+	fmt.Println("After payment you can find the invoice at this address:", invoiceAddress)
 }
